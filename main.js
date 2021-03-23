68cd8f2054bc4ddadb09adfac542249d2c3baa80
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-03-23 00:25:18 -0400

    updated for final solution before refactor

--- main.js
+++ main.js
@@ -37,6 +37,19 @@ const d12Button = document.querySelector("#d12-roll");
 const d20Button = document.querySelector("#d20-roll");
 const resetButton = document.querySelector("#reset-button");
 
+const d6Mean = document.querySelector("#d6-rolls-mean");
+const d6Median = document.querySelector("#d6-rolls-median");
+const d6Mode = document.querySelector("#d6-rolls-mode");
+const doubleD6Mean = document.querySelector("#double-d6-rolls-mean");
+const doubleD6Median = document.querySelector("#double-d6-rolls-median");
+const doubleD6Mode = document.querySelector("#double-d6-rolls-mode");
+const d12Mean = document.querySelector("#d12-rolls-mean");
+const d12Median = document.querySelector("#d12-rolls-median");
+const d12Mode = document.querySelector("#d12-rolls-mode");
+const d20Mean = document.querySelector("#d20-rolls-mean");
+const d20Median = document.querySelector("#d20-rolls-median");
+const d20Mode = document.querySelector("#d20-rolls-mode");
+
 /*******************
  * EVENT LISTENERS *
  *******************/
@@ -73,6 +86,21 @@ const resetAll = () => {
 /****************************
  * CLICK HANDLING FUNCTIONS *
  ****************************/
+const getImagePathD6 = (roll) => {
+  return `./images/d6/${roll}.png`;
+};
+
+const getImagePathNumbers = (roll) => {
+  return `./images/numbers/${roll}.png`;
+};
+
+const setText = (selector, text) => {
+  document.querySelector(selector).innerText = text;
+};
+
+const setSrc = (selector, src) => {
+  document.querySelector(selector).src = src;
+};
 
 const rollD6 = () => {
   const roll = getRandomNumber(6);
@@ -81,26 +109,101 @@ const rollD6 = () => {
   const mean = getMean(sixes);
   const mode = getMode(sixes);
 
-  d6Button.src = `./images/d6/${roll}.png`;
+  d6Button.src = getImagePathD6(roll);
   d6Mean.innerText = mean;
   d6Median.innerText = median;
   d6Mode.innerText = mode;
 };
 
+const rollDoubleD6 = () => {
+  const roll1 = getRandomNumber(6);
+  const roll2 = getRandomNumber(6);
+  // Push together for pushing into for scores
+  doubleSixes.push(roll1 + roll2);
+  const median = getMedian(doubleSixes);
+  const mean = getMean(doubleSixes);
+  const mode = getMode(doubleSixes);
+
+  doubleD6Button1.src = getImagePathD6(roll1);
+  doubleD6Button2.src = getImagePathD6(roll2);
+  doubleD6Mean.innerText = mean;
+  doubleD6Median.innerText = median;
+  doubleD6Mode.innerText = mode;
+};
+
+const rollD12 = () => {
+  const roll = getRandomNumber(12);
+  twelves.push(roll);
+  const median = getMedian(twelves);
+  const mean = getMean(twelves);
+  const mode = getMode(twelves);
+
+  d12Button.src = getImagePathNumbers(roll);
+  d12Mean.innerText = mean;
+  d12Median.innerText = median;
+  d12Mode.innerText = mode;
+};
+
+const rollD20 = () => {
+  const roll = getRandomNumber(20);
+  twenties.push(roll);
+  const median = getMedian(twenties);
+  const mean = getMean(twenties);
+  const mode = getMode(twenties);
+
+  d20Button.src = getImagePathNumbers(roll);
+  d20Mean.innerText = mean;
+  d20Median.innerText = median;
+  d20Mode.innerText = mode;
+};
+
+d6Button.addEventListener("click", rollD6);
+doubleD6Button1.addEventListener("click", rollDoubleD6);
+doubleD6Button2.addEventListener("click", rollDoubleD6);
+d12Button.addEventListener("click", rollD12);
+d20Button.addEventListener("click", rollD20);
+resetButton.addEventListener("click", resetAll);
+
 /****************
  * MATH SECTION *
  ****************/
-const d6Mean = document.querySelector("#d6-rolls-mean");
-const d6Median = document.querySelector("#d6-rolls-median");
-const d6Mode = document.querySelector("#d6-rolls-mode");
-const doubleD6Mean = document.querySelector("#double-d6-rolls-mean");
-const doubleD6Median = document.querySelector("#double-d6-rolls-median");
-const doubleD6Mode = document.querySelector("#double-d6-rolls-mode");
-const d12Mean = document.querySelector("#d12-rolls-mean");
-const d12Median = document.querySelector("#d12-rolls-median");
-const d12Mode = document.querySelector("#d12-rolls-mode");
-const d20Mean = document.querySelector("#d20-rolls-mean");
-const d20Median = document.querySelector("d20-rolls-median");
-const d20Mode = document.querySelector("#d20-rolls-mode");
+const getMean = (rolls) => {
+  let sum = 0;
+  // adds onto sum = total
+  for (const roll of rolls) {
+    sum += roll;
+  }
+  // take sum and divide by how many - shortens .2 after dot
+  return (sum / rolls.length).toFixed(2);
+};
 
-resetAll();  
+const getMedian = (rolls) => {
+  const sorted = sortByNumber(rolls);
+  const midPoint = Math.floor(sorted.length / 2);
+  // if 2 mediums add both divide by together
+  if (sorted.length % 2 === 0) {
+    return sorted[midPoint] + sorted[midPoint - 1];
+  } else {
+    return sorted[midPoint].toFixed(2);
+  }
+};
+
+const getMode = (rolls) => {
+  const counts = {};
+  let mode = "NA";
+  for (const roll of rolls) {
+    if (roll in counts) {
+      counts[roll]++;
+    } else {
+      counts[roll] = 1;
+    }
+
+    if (counts[mode] === undefined || counts[roll] > counts[mode]) {
+      mode = roll;
+    }
+  }
+  return mode;
+};
+
+
+resetAll();
